a43310023f4f3a2ef0e4fdb4df7a29461a0a78e8
Guillermo Lara <grlara9@gmail.com>
2020-05-29 23:38:07 -0700

    establish connection with mongodb atlas

--- server.js
+++ server.js
@@ -1,15 +1,21 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const mongoose = require('mongoose')
 
-var PORT = 5000;
+require('dotenv').config();
+
+var PORT = process.env.PORT || 5000;
 
 app.use(cors())
 app.use(express.json())
 
+const uri = process.env.ATLAS_URI;
+mongoose.connect( uri, {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true})
 
-app.get('/', function (req, res) {
-    res.send('hello world')
+const connection = mongoose.connection;
+connection.once('open', ()=>{
+    console.log("MongoDB database connection established successfully")
 })
 
 app.listen(PORT, ()=>{
