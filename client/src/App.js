71bf791bec9b5729d3d119b9a87b19ec84e397b5
Guillermo Lara <grlara9@gmail.com>
2020-06-01 10:56:21 -0700

    added lifecycle method to render all task from database

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,31 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import axios from 'axios'
 import AddTodo from './components/add.component'
 
-function App() {
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      todos:[]
+    }
+  }
+  componentDidMount(){
+    axios.get('http://localhost:5000/todo')
+    .then((response) => this.setState({
+      todos: response.data
+    })
+    )
+  }
+  render(){
   return (
     <div className="container">
       <AddTodo />
       
     </div>
   );
+}
 }
 
 export default App;
