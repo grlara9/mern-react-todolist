79fde235e1829767f19290947f27d8a8697f362b
Guillermo Lara <grlara9@gmail.com>
2020-05-31 21:12:53 -0700

    added state to add component and connect front to back end

--- client/src/components/add.component.js
+++ client/src/components/add.component.js
@@ -1,13 +1,45 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 class AddTodo extends Component {
+    constructor(props){
+        super(props);
+        this.state={
+            task:''
+        }
+    }
+    onchange =(e)=>{
+        const {name, value} = e.target;
+        this.setState({
+            [name]: value
+        })
+    }
+    onSubmit =(e) => {
+        e.preventDefault()
+
+        const todo ={
+            task: this.state.task,
+            completed: false
+        }
+        axios.post('http://localhost:5000/todo/add', todo )
+        .then(res => console.log("this is the dataa:" + res.data))
+
+        this.setState({
+            task: ''
+        })
+    }
+
     render() {
         return (
             <div>
-            <form>
+            <form onClick={this.onSubmit}>
                 <div className="form-group">
                 
-                <input type="text" className="form-control"  placeholder="Add To-do" />
+                <input type="text" className="form-control"  
+                name="task"
+                value={this.state.task}
+                onChange={this.onchange}
+                placeholder="Add To-do" />
                 </div>
                 <button type="submit" className="btn btn-primary">Submit</button>
             </form>
