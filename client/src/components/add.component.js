299517b026429ce5dabcb4ec01acf4ffe99043c3
Guillermo Lara <grlara9@gmail.com>
2020-05-31 19:09:07 -0700

    created two components add and todo

---
+++ client/src/components/add.component.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class AddTodo extends Component {
+    render() {
+        return (
+            <div>
+                this is the add component
+            </div>
+        )
+    }
+}
+export default AddTodo
