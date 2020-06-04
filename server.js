615fa5ef22154fac6f54287c5558f729109d6ce0
Guillermo Lara <grlara9@gmail.com>
2020-06-04 01:44:17 -0700

    initial

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var PORT = process.env.PORT || 5000;
 app.use(cors())
 app.use(express.json())
 
-const uri = process.env.ATLAS_URI;
+const uri = process.env.MONGODB_URI;
 mongoose.connect( uri, {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true})
 
 const connection = mongoose.connection;
