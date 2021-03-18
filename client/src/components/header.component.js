74ed14b50eac31c97aa1098bf8056ef81506f00a
Guillermo Lara <grlara9@gmail.com>
2021-03-17 21:00:36 -0700

    edit header to display different message dependent on number of todos

--- client/src/components/header.component.js
+++ client/src/components/header.component.js
@@ -3,9 +3,7 @@ import React from 'react'
 const Header = props =>(
     <div className="header">
       
-    <h1>
-      You have {props.num} Todos
-    </h1>
+  {props.num  ?  <h1>You have {props.num} Todos Left</h1> : <h1>Start Adding Todos for Today</h1> }
     </div>
   
 )
